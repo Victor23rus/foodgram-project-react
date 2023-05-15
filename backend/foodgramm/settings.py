6c38ca317d9c5595c7fa12e7bd43e94b983ba25f
Виктор Шилигин <Victor918@mail.ru>
2023-05-15 10:55:33 +0300

    v2

--- backend/foodgramm/settings.py
+++ backend/foodgramm/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = (
