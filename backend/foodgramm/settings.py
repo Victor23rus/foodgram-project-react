1b6efd0cb01bd7300e45d1ca64c8847a0d24dddb
Виктор Шилигин <Victor918@mail.ru>
2023-05-15 11:55:50 +0300

    v2

--- backend/foodgramm/settings.py
+++ backend/foodgramm/settings.py
@@ -128,7 +128,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = "/static/"
 
 MEDIA_URL = "/media/"
